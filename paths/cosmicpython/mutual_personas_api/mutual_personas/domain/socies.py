432a7d4ba69b86017ac3fe86092eb467572cb86f
Alvar Maciel <alvarmaciel@gmail.com>
2023-01-20 19:17:49 -0300

    Added tests and models for cuotes ans pay cuotes method

--- paths/cosmicpython/mutual_personas_api/mutual_personas/domain/socies.py
+++ paths/cosmicpython/mutual_personas_api/mutual_personas/domain/socies.py
@@ -1,9 +1,19 @@
-from typing import Optional
+from datetime import datetime
+from decimal import Decimal
+from typing import Dict, Optional, List
 import uuid
 from random import randint
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Cuote:
+    date:datetime
+    amount:Decimal
+
 
 class Socie:
     def __init__(self,
+                id: int,
                 nombre: str,
                 apellido:str,
                 dni: int,
@@ -12,9 +22,9 @@ class Socie:
                 direccion: str,
                 codigo_postal: str,
                 tipo_de_socio: int,
-                nro_uuid: Optional[uuid] = None,
-                nro: int =None,
+                nro_uuid: str,
                 ):
+        self.ide = id
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
@@ -23,9 +33,23 @@ class Socie:
         self.direccion = direccion
         self.codigo_postal = codigo_postal
         self.tipo_de_socio = tipo_de_socio
-        self.nro_uuid= nro_uuid if nro_uuid is not None else uuid.uuid1()
-        self.nro_de_socie = nro if nro is not None else randint(1, 1000)
+        self.nro_uuid= nro_uuid
+        self.cuotes=set()
+
+    @property
+    def activated(self)-> bool:
+        current_month = datetime.now().strftime('%m')
+        current_year_full = datetime.now().strftime('%Y')
+        if not self.cuotes:
+            return False
+        for cuote in self.cuotes:
+            if cuote.date.strftime('%Y')==current_year_full and cuote.date.strftime('%m') == current_month:
+                return True
+        return False
 
 
-
-
+    def pay_cuotes(self, cuotes:List[Cuote]):
+        current_month = datetime.now().strftime('%m')
+        current_year_full = datetime.now().strftime('%Y')
+        for cuote in cuotes:
+            self.cuotes.add(cuote)
--- paths/cosmicpython/mutual_personas_api/tests/domain/test_create_associate.py
+++ paths/cosmicpython/mutual_personas_api/tests/domain/test_create_associate.py
@@ -1,22 +1,12 @@
 import pytest
+from decimal import Decimal
 import uuid
-from mutual_personas.domain.socies import Socie
+from mutual_personas.domain.socies import Socie, Cuote
+from datetime import datetime, timedelta
 
-def test_new_socie():
-    """Variables de instancia de los socies
-        - Nombre
-        - Apellido
-        - DNI
-        - Nro de socie: Automatico
-        - Email
-        - Teléfono
-        - Dirección
-        - Codigo Postal
-        - tipo de Socio: Pleno(1) o General(0)
-        - Numero de socios: automático correlativo.
-        - uuid: clave única
-    """
-    # Given
+@pytest.fixture
+def new_socie(*args):
+    id= 1
     nombre = "Pepe"
     apellido = "Del Rio"
     dni = 27890098
@@ -25,20 +15,53 @@ def test_new_socie():
     direccion="una direccion"
     codigo_postal = "un codigo con 1234"
     tipo_de_socio= 1
-    uuid_number = uuid.uuid1()
+    uuid_number = str(uuid.uuid1())
 
-    # When
-    nuevo_socie = Socie(nombre, apellido, dni, email, telefono, direccion, codigo_postal, tipo_de_socio, uuid_number)
+
+    return Socie(id, nombre, apellido, dni, email, telefono, direccion, codigo_postal, tipo_de_socio, uuid_number)
+
+
+def test_new_socie(new_socie):
+
+    # Given
+    a_socie=new_socie
 
     # Then
-    assert nuevo_socie.nombre == nombre
-    assert nuevo_socie.apellido == apellido
-    assert nuevo_socie.dni == dni
-    assert isinstance(nuevo_socie.nro_de_socie, int)
-    assert nuevo_socie.email == email
-    assert nuevo_socie.telefono == telefono
-    assert nuevo_socie.direccion == direccion
-    assert nuevo_socie.codigo_postal == codigo_postal
-    assert nuevo_socie.tipo_de_socio == tipo_de_socio
-    assert nuevo_socie.nro_uuid == uuid_number
-    assert isinstance(nuevo_socie.nro_uuid, uuid.UUID)
+    assert isinstance(a_socie, Socie)
+
+
+def test_add_payed_actual_cuote_and_is_activated(new_socie):
+    # Given
+    now = datetime.now()
+    a_socie = new_socie
+    a_cuote = Cuote(now, Decimal(10.00))
+    another_cuote = Cuote(now - timedelta(days=31), Decimal(10.00))
+    cuotes=[a_cuote, another_cuote]
+    # When
+    a_socie.pay_cuotes(cuotes)
+    # Then
+    # There is same ammounts of cuotes in propertie cuotes than pay_cuotes
+    assert len(a_socie.cuotes) == len(cuotes)
+    assert a_socie.cuotes ==set(cuotes)
+    # the activated propertie is true
+    assert a_socie.activated
+
+def test_socie_has_older_cuotes_but_is_inactive(new_socie):
+    # Given
+    now = datetime.now()
+    a_socie = new_socie
+    a_cuote = Cuote(now - timedelta(days=61), Decimal(10.00))
+    another_cuote = Cuote(now - timedelta(days=31), Decimal(10.00))
+    cuotes=[a_cuote, another_cuote]
+    # When
+    a_socie.pay_cuotes(cuotes)
+    # Then
+    # There is same ammounts of cuotes in propertie cuotes than pay_cuotes
+    assert len(a_socie.cuotes) == len(cuotes)
+    assert a_socie.cuotes ==set(cuotes)
+    # the activated propertie is false
+    assert not a_socie.activated
+
+
+def socie_is_not_activated():
+    pass
