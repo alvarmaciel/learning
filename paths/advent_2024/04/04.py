67410ffbe6d136b735e52d34663529e02324fa8b
Alvar Maciel <alvarmaciel@gmail.com>
2025-09-20 17:59:01 -0300

    some old work

--- paths/advent_2024/04/04.py
+++ paths/advent_2024/04/04.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 def get_soup(file_input:str) -> str:
     matrix = []
 
@@ -12,30 +14,33 @@ def get_soup(file_input:str) -> str:
     return matrix
 
 def count_words(matrix: [[str]]) -> int:
-    rows = len(matrix)
-    col = len(matrix[0]) if rows > 0 else 0
-    word = 'XMAS'
-    reverse_word = 'SAMX'
+    col = len(matrix)
+    col = len(matrix[0]) if col > 0 else 0
+    word = 'MAS'
+    reverse_word = 'SAM'
     count = 0
     directions = [
-        (0, 1), # Derecha
-        (0, -1), # izquierda
-        (1, 0), # abajo
-        (-1, 0), # arriba
+        #(0, 1), # Derecha
+        #(0, -1), # izquierda
+        #(1, 0), # abajo
+        #(-1, 0), # arriba
         (1,1), # diagonal inferior derecha
         (-1,-1), # diagonal superior izquierda
         (1,-1), # Diagonal inferior izquierda
         (-1,1) # diagonal superior derecha
     ]
 
-    for i in range(rows):
+
+    for i in range(col):
         for j in range(col):
             for di, dj in directions:
                 matched = True
-                for k in range(4):
+                for k in range(3):
                     ni, nj = i + di * k, j + dj * k
-                    if 0 <= ni < rows and 0 <= nj < col:
-                        if matrix[ni][nj] != word[k]:
+                    if 0 <= ni < col and 0 <= nj < col:
+                        compared_word = matrix[ni][nj]
+                        word_to_compare = word[k]
+                        if compared_word  != word_to_compare:
                             matched = False
                             break
                     else:
@@ -60,6 +65,7 @@ def count_words(matrix: [[str]]) -> int:
     return count
 
 if __name__ == "__main__":
-    matrix = get_soup("input.txt")
+    input_path = Path(__file__).parent / "test_bis.txt"
+    matrix = get_soup(str(input_path))
     xmas_count = count_words(matrix)
-    print(xmas_count)
+    print(xmas_count/2)
